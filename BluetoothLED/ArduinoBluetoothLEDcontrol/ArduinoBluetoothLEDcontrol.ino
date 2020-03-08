f6e85d4dbba49f84464d2afcfa0ec5094f45cb4e
AcoptexCom <58400137+AcoptexCom@users.noreply.github.com>
2020-03-08 11:29:06 +0200

    Add files via upload

---
+++ BluetoothLED/ArduinoBluetoothLEDcontrol/ArduinoBluetoothLEDcontrol.ino
@@ -0,0 +1,30 @@
+/*
+Project: Bluetooth LED control
+Function: We are going to control red LED attached to the digital pin 10 of Arduino Uno via Blueatooth
+          through the Android App (made with MIT APP inventor 2)
+Credits: ACOPTEX.COM http://acoptex.com
+ */
+ 
+int ledPin = 10;//red LED attached to Arduino pin 10 
+int state;
+
+void setup() {              
+  Serial.begin(9600);   // initialize serial communication at 9600 bps
+  pinMode(ledPin, OUTPUT); // set the LED digital pin as an Output.    
+  
+}
+
+void loop() {
+  
+  if(Serial.available() > 0){     
+      state = Serial.read();}
+
+  if (state == '1') {
+      digitalWrite(ledPin, HIGH);}   // turn the red LED ON (The voltage level is HIGH)
+ 
+  else if (state == '2') {
+      digitalWrite(ledPin, LOW);}    // turn the LED OFF (The voltage level is LOW)
+      
+  delay(100);                     // wait for 100ms
+  
+}
